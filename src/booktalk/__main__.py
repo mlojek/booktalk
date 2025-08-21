9255a5c810d5c6f30475cff5f7c5f2b77eb0f457
mlojek <mlojek@protonmail.com>
2025-08-21 13:26:08 +0200

    Add pyproject and entrypoint script

---
+++ src/booktalk/__main__.py
@@ -0,0 +1,11 @@
+'''
+Entrypoint script for the booktalk project.
+'''
+
+
+def main():
+    '''
+    Entrypoint function for the booktalk project. This function is called when booktalk
+    is ran from CLI.
+    '''
+    print("Hello world!")
