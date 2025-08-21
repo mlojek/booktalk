e028cd75de6245685145081878a4ddc4bb1a540e
mlojek <mlojek@protonmail.com>
2025-08-21 21:44:59 +0200

    Use langchain Epub reader instead of my own

--- src/booktalk/__main__.py
+++ src/booktalk/__main__.py
@@ -6,23 +6,18 @@ import argparse
 from pathlib import Path
 
 from langchain_chroma import Chroma
+from langchain_community.document_loaders import UnstructuredEPubLoader
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_ollama import OllamaEmbeddings
 from langchain_ollama.llms import OllamaLLM
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-from .read_epub import read_epub_book
 
-
-# TODO add logger with INFO because the initialization is slow and user might
-# need some progress info
 def main():
     """
     Entrypoint function for the booktalk project.
     This function is called when booktalk is ran from CLI.
     """
-    print("Hello world!")
-
     parser = argparse.ArgumentParser(
         prog="booktalk",
         description="Run an interactive chat with AI to better understand a book.",
@@ -30,11 +25,11 @@ def main():
     parser.add_argument("book_path", type=Path, help="Path to an EPUB book.")
     args = parser.parse_args()
 
-    book = read_epub_book(args.book_path)
+    book = UnstructuredEPubLoader(args.book_path).load()
 
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=500)
 
-    book_chunks = text_splitter.split_documents([book])
+    book_chunks = text_splitter.split_documents(book)
 
     vector_database = Chroma.from_documents(
         documents=book_chunks,
