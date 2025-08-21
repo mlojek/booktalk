15a7a45a40420369db8d1bdae195d542126399a5
mlojek <mlojek@protonmail.com>
2025-08-21 17:31:08 +0200

    Add chroma db vector storage to the project

--- src/booktalk/__main__.py
+++ src/booktalk/__main__.py
@@ -8,9 +8,17 @@ from pathlib import Path
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_ollama.llms import OllamaLLM
 
+from langchain_ollama import OllamaEmbeddings
+from langchain_chroma import Chroma
+from langchain_core.documents import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+
 from .read_epub import read_epub_book
 
 
+# TODO add logger with INFO because the initialization is slow and user might
+# need some progress info
 def main():
     """
     Entrypoint function for the booktalk project. This function is called when booktalk
@@ -26,6 +34,29 @@ def main():
     args = parser.parse_args()
 
     book_contents = read_epub_book(args.book_path)
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=500)
+
+    book = Document(
+        page_content=book_contents,
+        metadata={},
+        id='the_book'
+    )
+
+
+    book_chunks = text_splitter.split_documents([book])
+
+
+    vector_database = Chroma.from_documents(
+        documents=book_chunks,
+        collection_name='book',
+        embedding=OllamaEmbeddings(model="mxbai-embed-large"),
+    )
+
+
+    chroma_retriever = vector_database.as_retriever()
+
+
 
     model = OllamaLLM(model="llama3.2")
 
@@ -45,9 +76,12 @@ def main():
         if question == "q":
             break
 
+
+        fragments = chroma_retriever.invoke(question, k=10)
+
         answer = chain.invoke(
             {
-                "fragments": [],
+                "fragments": fragments,
                 "question": question,
             }
         )
