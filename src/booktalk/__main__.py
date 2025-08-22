511750a580e5f53db6447c3cf2b5fcca52a622d6
mlojek <mlojek@protonmail.com>
2025-08-22 10:42:18 +0200

    Refactor main and take model name and k from CLI

--- src/booktalk/__main__.py
+++ src/booktalk/__main__.py
@@ -22,25 +22,38 @@ def main():
         prog="booktalk",
         description="Run an interactive chat with AI to better understand a book.",
     )
-    parser.add_argument("book_path", type=Path, help="Path to an EPUB book.")
+    parser.add_argument(
+        "book_path",
+        type=Path,
+        help="Path to an EPUB book.",
+    )
+    parser.add_argument(
+        "--model",
+        type=str,
+        help="Ollama LLM model to use (default: llama3.2).",
+        default="llama3.2",
+    )
+    parser.add_argument(
+        "--num_fragments",
+        type=int,
+        help="Number of relevant book fragment to use in RAG (default: 10).",
+        default=10,
+    )
     args = parser.parse_args()
 
+    # load the book, chunk it and load into chroma DB
     book = UnstructuredEPubLoader(args.book_path).load()
-
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=500)
-
     book_chunks = text_splitter.split_documents(book)
-
     vector_database = Chroma.from_documents(
         documents=book_chunks,
         collection_name="book",
         embedding=OllamaEmbeddings(model="mxbai-embed-large"),
     )
-
     chroma_retriever = vector_database.as_retriever()
 
-    model = OllamaLLM(model="llama3.2")
-
+    # create a prompt template and LLM chain
+    model = OllamaLLM(model=args.model)
     prompt = ChatPromptTemplate.from_template(
         """
         You are a helpful literature expert, which helps people with understanding books.
@@ -48,16 +61,16 @@ def main():
         Here is the user's question: {question}.
         """
     )
-
     chain = prompt | model
 
+    # in loop get the question from user and answer it using relevant context
     while True:
         question = input("Type your question (or q to quit): ")
 
         if question == "q":
             break
 
-        fragments = chroma_retriever.invoke(question, k=10)
+        fragments = chroma_retriever.invoke(question, k=args.num_fragments)
 
         answer = chain.invoke(
             {
@@ -65,4 +78,5 @@ def main():
                 "question": question,
             }
         )
+
         print("\n", answer, "\n")
